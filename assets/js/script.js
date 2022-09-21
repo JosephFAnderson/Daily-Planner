78a7290b34e8bc1ce620ac95bf0aea79132ab698
Joseph A <joseph_anderson11@yahoo.com>
2022-09-21 10:00:25 -0400

    Date displays

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,8 @@
 // Get the js file to upload
+var currentDate = new Date();
+var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var monthsOfYear = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+// console.log(currentDate);
+
+var dateEl = $("#currentDay");
+dateEl.text(daysOfWeek[currentDate.getDay()] + ", " + monthsOfYear[currentDate.getMonth()] + " " + currentDate.getDate());
