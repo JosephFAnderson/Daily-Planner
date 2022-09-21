aaab5181b43d62d724667dcbd603251446c1059c
Joseph A <joseph_anderson11@yahoo.com>
2022-09-21 18:47:30 -0400

    Handle save button click funtion

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,14 @@ var today = moment();
 var currentHour = today.hour();
 var rowEl = $(".customRow");
 var textAreaEl = $("textarea");
+var dayPlan = JSON.parse(localStorage.getItem("dayPlan")) || [];
 
+
+// Display current day
 $("#currentDay").text(today.format("dddd[, ]Do[ of ] MMMM"));
 
-rowEl.each(function (index) {
+// Color each time slot in relation to current time
+textAreaEl.each(function (index) {
     var rowTime = index + 9;
     if(currentHour > rowTime){
         $(textAreaEl[index]).addClass("past");
@@ -16,3 +20,37 @@ rowEl.each(function (index) {
         $(textAreaEl[index]).addClass("future");
     }
 });
+
+// Handler for clicking the save button
+function saveButtonHandler(e) {
+    var plan = {};    
+    
+    if($(e.target).is('i')) {
+        plan.eventText = $(e.target).parent('button').siblings('textarea').val()
+        plan.eventHour = $(e.target).parent('button').siblings('p').text()
+    }else {
+        plan.eventText = $(e.target).siblings('textarea').val();
+        plan.eventHour = $(e.target).siblings('p').text();
+    }
+
+    if (dayPlan.length === 0)
+    {
+        dayPlan.push(plan);
+    }else {
+        var doesContain = false;
+        for(var i = 0; i < dayPlan.length; i++) {
+            if (dayPlan[i].eventHour === plan.eventHour) {
+                dayPlan[i].eventText = plan.eventText
+                doesContain = true;
+            } 
+        }
+        
+        if(!doesContain) {
+            dayPlan.push(plan);
+        }
+    }
+    
+    localStorage.setItem("dayPlan", JSON.stringify(dayPlan));      
+}
+
+rowEl.on('click', '.saveBtn', saveButtonHandler);
