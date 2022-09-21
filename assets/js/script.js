d166630c5f66ca3a621ea65a9fd62a170c7bfe8f
Joseph A <joseph_anderson11@yahoo.com>
2022-09-21 16:38:41 -0400

    Color textarea based on time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
 // Display current day
 var today = moment();
+var currentHour = today.hour();
+var rowEl = $(".customRow");
+var textAreaEl = $("textarea");
+
 $("#currentDay").text(today.format("dddd[, ]Do[ of ] MMMM"));
+
+rowEl.each(function (index) {
+    var rowTime = index + 9;
+    if(currentHour > rowTime){
+        $(textAreaEl[index]).addClass("past");
+    }else if(currentHour === rowTime) {
+        $(textAreaEl[index]).addClass("present");
+    }else {
+        $(textAreaEl[index]).addClass("future");
+    }
+});
